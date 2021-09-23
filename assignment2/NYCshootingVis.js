068ba53efc36dc38e164c1973ff7d04744814d48
jasonkknipe <50972526+jasonkknipe@users.noreply.github.com>
2021-09-23 13:48:13 +1000

    beaten choropleth

--- assignment2/NYCshootingVis.js
+++ assignment2/NYCshootingVis.js
@@ -1,4 +1,4 @@
-var vg_1 = "NYCvis.json";
+var vg_1 = "NYCchoropleth.json";
 vegaEmbed("#NYCmap", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
