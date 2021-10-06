d63a6dd573faa367f7083de5db003b61e988ec51
jasonkknipe <50972526+jasonkknipe@users.noreply.github.com>
2021-10-06 15:09:15 +1100

    updated variable names

--- assignment2/homework10.js
+++ assignment2/homework10.js
@@ -1,4 +1,4 @@
 var vg_1 = "hw10.json";
-vegaEmbed("#mapWeek10", vg_1).then(function(result) {
+vegaEmbed("#pieWeek10", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
