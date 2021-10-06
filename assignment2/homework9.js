57730d1a23386da07f6dd14adc0856d1b7a9b36c
jasonkknipe <50972526+jasonkknipe@users.noreply.github.com>
2021-10-06 13:48:07 +1100

    refactored files

--- assignment2/homework9.js
+++ assignment2/homework9.js
@@ -1,4 +1,4 @@
-var vg_1 = "NYCchoropleth.json";
-vegaEmbed("#NYCmap", vg_1).then(function(result) {
+var vg_1 = "hw9.json";
+vegaEmbed("#mapWeek9", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
