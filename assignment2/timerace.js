6aa6d01e4ca3bccd07bcf5febea2cccef15c2953
jasonkknipe <50972526+jasonkknipe@users.noreply.github.com>
2021-10-14 11:13:14 +1100

    fghnfghn

--- assignment2/timerace.js
+++ assignment2/timerace.js
@@ -1,4 +1,4 @@
 var vg_1 = "timerace.json";
-vegaEmbed("#timerace", vg_1).then(function(result) {
+vegaEmbed("#timeRace", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
